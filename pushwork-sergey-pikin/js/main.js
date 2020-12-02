0d449771fd7a9dbb1ae8700d8555f7aa61c79980
lghiggino <lghiggino@gmail.com>
2020-12-02 18:32:35 -0300

    javascript for hero button classlist.add

--- pushwork-sergey-pikin/js/main.js
+++ pushwork-sergey-pikin/js/main.js
@@ -1,1 +1,12 @@
+console.log("javascript is on")
 
+const heroButton = document.querySelector("#hero button")
+
+window.addEventListener("resize", () => {
+    console.log(`current window width is ${window.innerWidth}`);
+    if(window.innerWidth <= 648){
+        heroButton.classList.add("button-inverted");
+    }else{
+        heroButton.classList.remove("button-inverted");
+    }
+})
