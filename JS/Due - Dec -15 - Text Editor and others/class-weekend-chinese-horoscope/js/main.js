656abff79a57016842766de1b61638dbd04cc029
lghiggino <lghiggino@gmail.com>
2020-12-14 06:47:56 -0300

    chinese sign picker

---
+++ JS/Due - Dec -15 - Text Editor and others/class-weekend-chinese-horoscope/js/main.js
@@ -0,0 +1,86 @@
+// click event on the button
+const button = document.getElementById("check")
+button.addEventListener("click", checkHoroscope)
+
+//place to see theresult
+const placeToSee = document.getElementById("placeToSee")
+
+function checkHoroscope(){
+  //check the user input date
+  const birthDate = document.querySelector("#day").value;
+
+  //split the string into day, month and year
+  const year = Number.parseInt(birthDate.slice(0, 4))
+  const month = Number.parseInt(birthDate.slice(5, 7))
+  const day = Number.parseInt(birthDate.slice(8, birthDate.length))
+  console.log("year:", year, "month:", month,"day:", day)
+
+  //determine the chineseYear
+  let chineseYear = year % 12
+
+  //checking the exceptions
+  for (i = 0; i < exceptions.length; i++){
+    if ((year === exceptions[i][0]) && (month === exceptions[i][1]) && (day <= exceptions[i][2])){
+      chineseYear--
+    }
+  }
+
+  switch(true){
+    case (chineseYear === 0):
+      placeToSee.innerText = "Monkey"
+      renderSignInfo(monkey)
+      break;
+    case (chineseYear === 1):
+      placeToSee.innerText = "Rooster"
+      renderSignInfo(rooster)
+      break;
+    case (chineseYear === 2):
+      placeToSee.innerText = "Dog"
+      renderSignInfo(dog)
+      break;
+    case (chineseYear === 3):
+      placeToSee.innerText = "Pig"
+      renderSignInfo(pig)
+      break;
+    case (chineseYear === 4):
+      placeToSee.innerText = "Rat"
+      renderSignInfo(rat)
+      break;
+    case (chineseYear === 5):
+      placeToSee.innerText = "Ox"
+      renderSignInfo(ox)
+      break;
+    case (chineseYear === 6):
+      placeToSee.innerText = "Tiger"
+      renderSignInfo(tiger)
+      break;  
+    case (chineseYear === 7):
+      placeToSee.innerText = "Rabbit"
+      renderSignInfo(rabbit)
+      break;
+    case (chineseYear === 8):
+      placeToSee.innerText = "Dragon"
+      renderSignInfo(dragon)
+      break;
+    case (chineseYear === 9):
+      placeToSee.innerText = "Snake"
+      renderSignInfo(snake)
+      break;
+    case (chineseYear === 10):
+      placeToSee.innerText = "Horse"
+      renderSignInfo(horse)
+      break;
+    case (chineseYear === 11):
+      placeToSee.innerText = "Sheep"
+      renderSignInfo(sheep)
+      break; 
+  }
+
+  //compare its year to see chinese
+
+}
+
+function renderSignInfo(sign){
+  console.log(sign)
+}
+
---
+++ JS/Due - Dec -15 - Text Editor and others/class-weekend-chinese-horoscope/js/signinfo.js
@@ -0,0 +1,169 @@
+
+
+const rat = {
+    recentYears: "Recent Rat years are 1948, 1960, 1972, 1984, 1996, 2008, and 2020.",
+    order: "Year of the Rat, or year of the Bat and Bad? The recent Corona Virus ourbreaking in Wuhan China seems to be a sign of the beginnings of an inauspicious year, according to Chinese beliefs, for those living in China and especially for those living in Wuhan where the virus originated. Some recent reports have indicated that the virus may have come from close contact with bats - who have unwillingly taken over the headlines during this new Year of the Rat... We hope that all people who are Rats in the Chinese zodiac aren't too discouraged - This is your year! - To all Rats around the world, we wish you a healthy and lucky year in 2020. The Year of the Rat is the first zodiac sign in the Chinese zodiac cycle. According to the Chinese zodiac story, in the competition held by the Jade Emperor to decide the zodiac animals, the quick-witted rat asked the diligent ox to take him on a ride to cross the river and jumped down before the ox crossed the finish line, so the rat won the race and became the first of the zodiac animals.",
+    personality0: "Rats are quick-witted, resourceful, and smart but lack courage. With rich imaginations and sharp observations, they can take advantage of various opportunities well.",
+    personality1: "In Chinese culture, rats represent working diligently and thriftiness, so people born in a Rat year are thought to be wealthy and prosperous.",
+    personality2: "Men born in a Rat year are very curious so they tend to try their hands at anything and they can deal with tasks skillfully.",
+    personality3: "Women born in a Rat year are pretty, smart, and lovely. They have quick minds and dexterous hands, and are able to learn anything. They are always considerate of their families and friends.",
+    luckyNumbers: "2 and 3",
+    luckyColors: "blue, gold, green",
+    luckyFlowers: "lilly and african violet",
+    luckyDays:"4th and 13th",
+    luckyMonths:"2nd, 25th and 9th",
+}
+
+const ox = {
+    recentYears: "The recent zodiac years of Ox sign are: 1961, 1973, 1985, 1997, 2009, 2021, 2033…An Ox year occurs every 12 years.",
+    order: "2021 is a year of the Ox, starting from February 12th, 2021 (Chinese lunar New Year Day) and lasting until January 30th, 2022. It will be a Metal Ox year.",
+    personality0: "Having an honest nature, Oxes are known for diligence, dependability, strength and determination. These reflect traditional conservative characteristics.",
+    personality1: "Women Oxes are traditional, faithful wives, who attach great importance to their children's education.",
+    personality2: "For male oxes, they are strongly patriotic, have ideals and ambitions for life, and attach importance to family and work.",
+    personality3: "Having great patience and a desire to make progress, Oxes can achieve their goals by consistent effort. They are not much influenced by others or the environment, but persist in doing things according to their ideals and capabilities. Before taking any action, Oxes will have a definite plan with detailed steps, to which they apply their strong faith and physical strength. As a result, people of the Ox zodiac sign often enjoy great success. Oxes are weakest in their communication skills. They are not good at communicating with others, and even think it is not worthwhile to exchange ideas with others. They are stubborn and stick to their own ways.",
+    luckyNumbers: "1 and 4",
+    luckyColors: "white, yellow, green",
+    luckyFlowers: "tulip and peach blossoms",
+    luckyDays:"13th and 27th",
+    luckyMonths:"7th and 9th",
+}
+
+const tiger = {
+    recentYears: "Years of the Tiger: 1926, 1938, 1950, 1962, 1974, 1986, 1998, 2010, 2022. In 2022, it will be the year of the Chinese Zodiac Tiger, it starts from the Chinese lunar New Year (February 1).",
+    order: "The Tiger ranks third among the animals in the Chinese zodiac. The 12 zodiac animals are, in order: Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, and Pig. Each year is related to an animal sign according to a 12-year-cycle.",
+    personality0: "People born in a year of the Tiger are brave, competitive, unpredictable, and confident. They are very charming and well-liked by others. But sometimes they are likely to be impetuous, irritable, and overindulged.",
+    personality1: "With stubborn personalities and tough judgment, Tigers work actively and express themselves boldly, doing things in a high-handed manner. They are authoritative and never go back on what they have said.",
+    personality2: "With great confidence and indomitable fortitude, they can be competent leaders. They will not make preparations for anything, but can handle anything that comes along.",
+    personality3: "The best careers for Tigers are: advertising agent, office manager, travel agent, actor, writer, artist, pilot, flight attendant, musician, comedian, and chauffeur.",
+    luckyNumbers: "1, 3 and 4",
+    luckyColors: "blue, gray, orange",
+    luckyFlowers: "yellow lilly and cineraria",
+    luckyDays:"16th and 27th",
+    luckyMonths:"3rd, 7th and 10th",
+}
+
+const rabbit = {
+    recentYears: "Years of the Rabbit: 2023, 2011, 1999, 1987, 1975, 1963, 1951, 1939, 1927...The next Rabbit year is 2023. It starts on Chinese New Year (January 22).",
+    order: "The Rabbit is rank fourth in the Chinese zodiac cycle. The 12 zodiac animals are, in order: Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, and Pig. A Rabbit year occurs every 12 years.",
+    personality0: "Rabbits tend to be gentle, quiet, elegant, and alert as well as quick, skillful, kind, patient, and very responsible. However, they might be superficial, stubborn, melancholy, and overly-discreet. ",
+    personality1: "Gentlemen who belong to the Rabbit zodiac sign always treat people politely, with a gentle smile that makes people feel that they are credible and sincere. ",
+    personality2: "Ladies belonging to the Rabbit zodiac sign, apart from having a pretty and demure appearance, have a pure heart.",
+    personality3: "Rabbits are faithful to those around them but are reluctant to reveal their minds to others, and have a tendency to escape reality. They are too cautious and conservative, which means they miss good opportunities.",
+    luckyNumbers: "3, 4 and 6",
+    luckyColors: "pink, red, purple and blue",
+    luckyFlowers: "plantain lilly and jasmine",
+    luckyDays:"26th, 27th and 29th",
+    luckyMonths:"1st, 4th, 8th and 11th",
+}
+
+const dragon = {
+    recentYears: "Years of the Dragon: 1928, 1940, 1952, 1964, 1976, 1988, 2000, 2012, and 2024.",
+    order: "The Dragon is the fifth of the 12 Chinese zodiac animals. Each year has an animal sign according to the 12-year cycle. The next Dragon Year is 2024.",
+    personality0: "Among the Chinese zodiac animals, the dragon is the sole imaginary animal. The Chinese dragon is the most vital and powerful beast in the Chinese zodiac, although Dragons have an infamous reputation for being a hothead and possessing a sharp tongue. ",
+    personality1: "In ancient times, people thought that Dragons were best suited to be leaders of the world with their character traits of dominance and ambition.",
+    personality2: "Gifted with innate courage, tenacity, and intelligence, Dragons are enthusiastic and confident. They are not afraid of challenges, and willing to take risks.",
+    personality3: "However, Dragons are sometimes regarded as aggressive, and angry Dragons are not open to criticism. They don't consider themselves irritating and arrogant. Instead of following tradition, they strive for a smooth future.",
+    luckyNumbers: "1, 6 and 7",
+    luckyColors: "gold, silver and grayish white",
+    luckyFlowers: "bleeding-heart globy bower and dragong flowers",
+    luckyDays:"1st and 16th",
+    luckyMonths:"3rd, 4th and 7th",
+}
+
+const snake = {
+    recentYears: "Years of the Snake: 1917, 1929, 1941, 1953, 1965, 1977, 1989, 2001, 2013, and 2025.",
+    order: "The Snake occupies the sixth position in the Chinese zodiac. The 12 zodiac animals are, in order: Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, and Pig. Each year is related to a Chinese zodiac animal according to a 12-year-cycle.",
+    personality0: "In Chinese culture, the Snake is the most enigmatic animal among the 12 zodiac animals. People born in a year of the Snake are supposed to be the most intuitive.",
+    personality1: "Snakes tend to act according to their own judgments while remaining private and reticent. They are determined to accomplish their goals and hate to fail.",
+    personality2: "Snakes represent the symbol of wisdom. They are intelligent and wise. They are good at communication but say little. Snakes are usually regarded as great thinkers.",
+    personality3: "Snake people prefer to work alone; therefore, they are easily stressed. If they seem unusually stressed, it is best to allow them their own space and time to return to normal.",
+    luckyNumbers: "2, 8 and 9",
+    luckyColors: "black, red, yellow",
+    luckyFlowers: "orchid and cactuss",
+    luckyDays:"1st and 23rd",
+    luckyMonths:"1st, 8th and 11th",
+}
+
+const horse = {
+    recentYears: "Years of the Horse include 1930, 1942, 1954, 1966, 1978, 1990, 2002, 2014, and 2026.",
+    order: "The Horse occupies the seventh position in the Chinese zodiac, after the Snake and before the Goat. Horse years recur according to the Chinese zodiac 12-year cycle.",
+    personality0: "People born in a year of the Horse are extremely animated, active and energetic. Horses love to be in a crowd, and they can usually be seen on such occasions as concerts, theater performances, meetings, sporting events, and parties.",
+    personality1: "Sometimes, the Horse is a little self-centered, but it doesn't mean that she/he ins't interested in others' problems. ",
+    personality2: "",
+    personality3: "",
+    luckyNumbers: "2, 3 and 7",
+    luckyColors: "green and yellow",
+    luckyFlowers: "calla lilly and jasmine",
+    luckyDays:"5th and 20th",
+    luckyMonths:"4th, 9th and 12th",
+}
+
+const goat = {
+    recentYears: "Years of the Goat: 1931, 1943, 1955, 1967, 1979, 1991, 2003, 2015, 2027.",
+    order: "The Goat is eighth in the Chinese zodiac cycle. According to Chinese astrology, each year is associated with an animal sign, occurring in a 12-year cycle. ",
+    personality0: "People born in a year of the Goat are generally believed to be gentle, mild-mannered, shy, stable, sympathetic, amicable, and brimming with a strong sense of kindheartedness and justice.",
+    personality1: "Although they prefer to be in groups, they do not want to be the center of attention. They are reserved and quiet, most likely because they like spending a lot of time in their own thoughts. Goats like to spend money on fashionable things that give them a first class appearance. Although Goats enjoy spending money on the finer things in life, they are not snobbish.",
+    personality2: "",
+    personality3: "",
+    luckyNumbers: "2 and 7",
+    luckyColors: "brown red and purple",
+    luckyFlowers: "carnations and primroses",
+    luckyDays:"7th and 30th",
+    luckyMonths:"",
+}
+
+const monkey = {
+    recentYears: "Recent years of the Monkey: 2018, 2016, 2004, 1992, 1980, 1968, 1956.",
+    order: "The Monkey is the ninth of 12 animals in the recurring 12-year Chinese zodiac cycle. The 12 zodiac animals are, in order: Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, and Pig. A Monkey year occurs every 12 years",
+    personality0: "People born in a year of the Monkey have magnetic personalities and are witty and intelligent. Personality traits like mischievousness, curiosity, and cleverness make them very naughty.",
+    personality1: "Monkeys are masters of practical jokes, because they like playing most of the time. Although they don't have bad intentions, their pranks sometimes hurt other people's feelings.",
+    personality2: "Monkeys are fast learners and crafty opportunists. They have many interests and need partners who are capable of stimulating them. While some like the eccentric nature of Monkeys, others don't trust their sly, restless, and inquisitive nature.",
+    personality3: "Although they are clever and creative, Monkeys can't always exhibit their talents properly. They like to accept challenges and prefer urban life to rural life.",
+    luckyNumbers: "4 and 9",
+    luckyColors: "white, blue, gold",
+    luckyFlowers: "chrysanthemum and crape-myrtle",
+    luckyDays:"14th and 28th",
+    luckyMonths:"8th and 12th",
+}
+
+const rooster = {
+    recentYears: "Recent years of the Rooster include 2029, 2017, 2005, 1993, 1981, 1969, 1957, and 1945.",
+    order: "The Rooster is 10th among the 12 animals in the Chinese zodiac cycle. Every 12 years is a Rooster year, beginning at Chinese New Year.",
+    personality0: "People born in a year of the Rooster are very observant. Hardworking, resourceful, courageous, and talented, Roosters are very confident in themselves.",
+    personality1: "Roosters are always active, amusing, and popular within a crowd. Roosters are talkative, outspoken, frank, open, honest, and loyal individuals. They like to be the center of attention and always appear attractive and beautiful.",
+    personality2: "Roosters are happiest when they are surrounded by others, whether at a party or just a social gathering. They enjoy the spotlight and will exhibit their charm on any occasion.",
+    personality3: "",
+    luckyNumbers: "5, 7 and 8",
+    luckyColors: "gold, brown and yellow",
+    luckyFlowers: "gladiola, cockscomb",
+    luckyDays:"4th and 26th",
+    luckyMonths:"2nd, 5th, and 11th",
+}
+
+const dog = {
+    recentYears: "Recent years of the Dog include 1934, 1946, 1958, 1970, 1982, 1994, 2006, 2018, and 2030... 2018 is classed as an Earth Dog year.",
+    order: "The Dog occupies the 11th position in the Chinese zodiac after the Rooster and before the Pig. In Chinese astrology, each year is related to a Chinese zodiac animal according to a 12-year cycle.",
+    personality0: "Dogs are loyal and honest, amiable and kind, cautious and prudent. Due to having a strong sense of loyalty and sincerity, Dogs will do everything for the person who they think is most important.",
+    personality1: "As Dogs are not good at communication, it is difficult for them to convey their thoughts to others. Therefore, Dogs tend to leave others with the impression that they have a stubborn personality.",
+    personality2: "",
+    personality3: "",
+    luckyNumbers: "3, 4 and 9",
+    luckyColors: "red, green, and purple",
+    luckyFlowers: "rose, cymbidium orchids",
+    luckyDays:"7th and 28th",
+    luckyMonths:" 6th, 10th, and 12th",
+}
+
+const pig = {
+    recentYears: "Recent years of the Pig include 1935, 1947, 1959, 1971, 1983, 1995, 2007, 2019, and 2031.",
+    order: "2019 is a Year of the Pig according to the Chinese zodiac, and it's an Earth Pig year. In Chinese astrology, each year belongs to a Chinese zodiac animal according to the 12-year cycle.",
+    personality0: "Pigs are diligent, compassionate, and generous. They have great concentration: once they set a goal, they will devote all their energy to achieving it. Though Pigs rarely seek help from others, they will not refuse to give others a hand. Pigs never suspect trickery, so they are easily fooled.",
+    personality1: "General speaking, Pigs are relatively calm when facing trouble. No matter how difficult the problems are Pigs encounter, they can handle things properly and carefully. They have a great sense of responsibility to finish what they are engaged in.",
+    personality2: "",
+    personality3: "",
+    luckyNumbers: "2, 5 and 8",
+    luckyColors: "yellow, gray, brown, gold",
+    luckyFlowers: "hydrangea and daisy",
+    luckyDays:"17th and 24th",
+    luckyMonths:"2nd, 7th, 10th, and 11th",
+}
