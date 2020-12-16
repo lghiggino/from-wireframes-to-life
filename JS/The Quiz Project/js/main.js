77b1f27e48a4f1dc4863d3184ad4d7161f6ab144
lghiggino <lghiggino@gmail.com>
2020-12-16 19:53:50 -0300

    database

--- JS/The Quiz Project/js/main.js
+++ JS/The Quiz Project/js/main.js
@@ -1,23 +1,37 @@
-//question1 consts
-const question1Submit = document.querySelector("#question1-submit");
-const question1Result = document.querySelector("#question1-result");
-const question1MdnLink = `<a href="https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector" target="blank">query selector</a>`
-question1Submit.addEventListener("click", question1checker)
+//question0 consts
+const question0Submit = document.querySelector("#question0-submit");
+const question0Result = document.querySelector("#question0-result");
+const question0MdnLink = `<a href="https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector" target="blank">query selector</a>`
+question0Submit.addEventListener("click", question0checker)
 
-//question2 consts...
-const question2 = document.querySelector("#question2")
+//question1 consts...
+const question1 = document.querySelector("#question1")
 
 
 
-function question1checker(){
-    const question1Answer = document.querySelector("#question1-answer").value;
-    console.log(question1Answer)
-    if (question1Answer.includes("document.querySelector")){
-        question1Result.innerText = "This is the correct answer"
-        question2.classList.remove("hidden")
+function question0checker(){
+    const question0Answer = document.querySelector("#question0-answer").value;
+    console.log(question0Answer)
+    if (question0Answer.includes("document.querySelector")){
+        question0Result.innerText = "This is the correct answer"
+        question1.classList.remove("hidden")
     } else {
-        question1Result.innerHTML = `
-        check this MDN article : ${question1MdnLink}`
+        question0Result.innerHTML = `
+        check this MDN article : ${question0MdnLink}`
         
     }
 }
+
+
+const database = [
+    {"question0-question": "How to setup a query Selector",
+     "question0Answer": "document.querySelector",
+     "question0ResultCorrect": "This is the correct answer",
+     "question0ResultWrong" : `<a href="https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector" target="blank">query selector</a>`,
+     "question0NextQuestion": "question1"},
+     {"question1-question": "How to add an event listener",
+     "question1Answer": "addEventListener",
+     "question1ResultCorrect": "This is the correct answer",
+     "question1ResultWrong" : `<a href="https://developer.mozilla.org/en-US/docs/Web/API/Document/eventListener" target="blank">event listener</a>`,
+     "question1NextQuestion": "question2"},
+]
