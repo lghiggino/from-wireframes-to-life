63b3d9ff8fb6b23f1d905512410880a067279e47
lghiggino <lghiggino@gmail.com>
2020-12-16 19:24:02 -0300

    Kye's quiz project

--- JS/The Quiz Project/js/main.js
+++ JS/The Quiz Project/js/main.js
@@ -1,1 +1,23 @@
+//question1 consts
+const question1Submit = document.querySelector("#question1-submit");
+const question1Result = document.querySelector("#question1-result");
+const question1MdnLink = `<a href="http://www.mdn.org" target="blank">query selector</a>`
+question1Submit.addEventListener("click", question1checker)
 
+//question2 consts...
+const question2 = document.querySelector("#question2")
+
+
+
+function question1checker(){
+    const question1Answer = document.querySelector("#question1-answer").value;
+    console.log(question1Answer)
+    if (question1Answer.includes("document.querySelector")){
+        question1Result.innerText = "This is the correct answer"
+        question2.classList.remove("hidden")
+    } else {
+        question1Result.innerText = `
+        check this MDN article : ${question1MdnLink}`
+        
+    }
+}
