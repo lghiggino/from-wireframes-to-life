b3458ff382b7a25c89499e40b3cf1585b139f299
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-01-22 22:28:51 -0300

    codewars baybay

--- Daily Coding Challenge/Due_2021-01-29.js
+++ Daily Coding Challenge/Due_2021-01-29.js
@@ -28,3 +28,30 @@ function getCount(str) {
 
 getCount("banana")
 getCount("xzz")
+
+
+//Mumbling
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+
+
+// best solution
+function accumBest(string){
+  return string.split("").map((char, idx) => (char.toUpperCase() + char.toLowerCase().repeat(idx))).join("-")
+}
+//my solution
+function accum(string) {
+  let newString = ""
+  for (i = 0; i < string.length; i++){
+    newString += `${string[i].toUpperCase()}${string[i].toLowerCase().repeat(i)}-`
+  }
+  return newString.slice(0, -1)
+}
+
+console.log(accum("RqaEzty"))
+
+// Highest and Lowest
+// Test.assertEquals(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214");
+function highAndLow(numbers){
+  return `${Math.max(...numbers.split(" "))} ${Math.min(...numbers.split(" "))}`
+}
