ff5e9b91682280c884f52013e8092fe79094e8ad
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-02-02 14:46:07 -0300

    api1 and api2 are working, css and html base work

--- Due - Feb-04-2021/FoodAndBeveragePairs/js/main.js
+++ Due - Feb-04-2021/FoodAndBeveragePairs/js/main.js
@@ -1,53 +1,46 @@
 
-// let drinks = 'https://api.punkapi.com/v2/beers?'
+let drinks = 'https://api.punkapi.com/v2/beers?'
 
-// fetch(drinks)
-//     .then(res => res.json()) // parse response as JSON
-//     .then(data => {
-//       //console.log(data)
-//       data.forEach(brew => {
-//         console.log(brew)
-//         console.log(brew.food_pairing[0])
-//         let foods1 = `https://www.themealdb.com/api/json/v1/1/search.php?s=${brew.food_pairing[0]}`
-//         let foods2 = `https://www.themealdb.com/api/json/v1/1/search.php?s=${brew.food_pairing[1]}`
-//         let foods3 = `https://www.themealdb.com/api/json/v1/1/search.php?s=${brew.food_pairing[2]}`
-//         fetch(foods1)
-//           .then(res => res.json())
-//           .then(data => {
-//             console.log(data)
-//           })
-//           .catch(err => console.log(err))
-//         fetch(foods2)
-//           .then(res => res.json())
-//           .then(data => {
-//             console.log(data)
-//           })
-//           .catch(err => console.log(err))
-//         fetch(foods3)
-//           .then(res => res.json())
-//           .then(data => {
-//             console.log(data)
-//           })
-//           .catch(err => console.log(err))
-//       })
-//     })
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//     });
+fetch(drinks)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      //console.log(data)
+      data.slice(1,2).forEach(brew => {
+        console.log(brew)
+        console.log(brew.food_pairing[0], brew.food_pairing[1], brew.food_pairing[2])
+            fetch(`https://api.edamam.com/search?q=${brew.food_pairing[0]}&app_id=ef2a1231&app_key=b5a595d2db9574be00b6f9d446c03184`)
+            .then(res => res.json())
+            .then(data => {
+              console.log(data.hits)
+            })
+            .catch(err => console.log(err))
+            fetch(`https://api.edamam.com/search?q=${brew.food_pairing[1]}&app_id=ef2a1231&app_key=b5a595d2db9574be00b6f9d446c03184`)
+            .then(res => res.json())
+            .then(data => {
+              console.log(data.hits)
+            })
+            .catch(err => console.log(err))
+            fetch(`https://api.edamam.com/search?q=${brew.food_pairing[2]}&app_id=ef2a1231&app_key=b5a595d2db9574be00b6f9d446c03184`)
+            .then(res => res.json())
+            .then(data => {
+              console.log(data.hits)
+            })
+            .catch(err => console.log(err))
+      })
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
 
 
 
 
-//FOODS
-// Optional Parameters:
-// i : comma delimited ingredients
-// q : normal search query
-// p : page
-// format=xml : if you want xml instead of json 
 
-fetch("https://api.edamam.com/search?q=chicken&app_id=ef2a1231&app_key=b5a595d2db9574be00b6f9d446c03184")
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-  })
-  .catch(err => console.log(err))
+
+//EDAMAN BASIC FETCH
+// fetch(`https://api.edamam.com/search?q=chicken&app_id=ef2a1231&app_key=b5a595d2db9574be00b6f9d446c03184`)
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data.hits)
+//   })
+//   .catch(err => console.log(err))
