b5e46be98232f3a537b6eef01d642e7739516642
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-02-02 10:08:58 -0300

    fetch works at edamam

--- Due - Feb-04-2021/FoodAndBeveragePairs/js/main.js
+++ Due - Feb-04-2021/FoodAndBeveragePairs/js/main.js
@@ -1,25 +1,39 @@
 
-let drinks = 'https://api.punkapi.com/v2/beers?'
+// let drinks = 'https://api.punkapi.com/v2/beers?'
 
-fetch(drinks)
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      //console.log(data)
-      data.forEach(brew => {
-        console.log(brew)
-        console.log(brew.food_pairing[0])
-        let foods = `https://www.themealdb.com/api/json/v1/1/search.php?s=${brew.food_pairing[0]}`
-        fetch(foods)
-          .then(res => res.json())
-          .then(data => {
-            console.log(data)
-          })
-          .catch(err => console.log(err))
-      })
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
-    });
+// fetch(drinks)
+//     .then(res => res.json()) // parse response as JSON
+//     .then(data => {
+//       //console.log(data)
+//       data.forEach(brew => {
+//         console.log(brew)
+//         console.log(brew.food_pairing[0])
+//         let foods1 = `https://www.themealdb.com/api/json/v1/1/search.php?s=${brew.food_pairing[0]}`
+//         let foods2 = `https://www.themealdb.com/api/json/v1/1/search.php?s=${brew.food_pairing[1]}`
+//         let foods3 = `https://www.themealdb.com/api/json/v1/1/search.php?s=${brew.food_pairing[2]}`
+//         fetch(foods1)
+//           .then(res => res.json())
+//           .then(data => {
+//             console.log(data)
+//           })
+//           .catch(err => console.log(err))
+//         fetch(foods2)
+//           .then(res => res.json())
+//           .then(data => {
+//             console.log(data)
+//           })
+//           .catch(err => console.log(err))
+//         fetch(foods3)
+//           .then(res => res.json())
+//           .then(data => {
+//             console.log(data)
+//           })
+//           .catch(err => console.log(err))
+//       })
+//     })
+//     .catch(err => {
+//         console.log(`error ${err}`)
+//     });
 
 
 
@@ -30,3 +44,10 @@ fetch(drinks)
 // q : normal search query
 // p : page
 // format=xml : if you want xml instead of json 
+
+fetch("https://api.edamam.com/search?q=chicken&app_id=ef2a1231&app_key=b5a595d2db9574be00b6f9d446c03184")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err => console.log(err))
