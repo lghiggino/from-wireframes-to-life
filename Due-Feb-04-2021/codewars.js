6292c95c6e4d28ee39fc0d7919f345664d50c353
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-02-04 18:47:06 -0300

    f****** codewars sometimes

--- Due-Feb-04-2021/codewars.js
+++ Due-Feb-04-2021/codewars.js
@@ -486,11 +486,26 @@ function divisors(integer) {
 };
 
 
-//Feb-03-2021 - 28
-//
+//Feb-04-2021 - 28
+//Get the mean of an array
+function getAverage(marks){
+  let reduced = marks.reduce( (acc,curr) => acc +curr)
+  return Math.trunc(reduced/marks.length)
+}
 
-//Feb-03-2021 - 29
-//
+//Feb-04-2021 - 29
+//Do I get a bonus?
+function bonusTime(salary, bonus) {
+  if(!bonus) return `£${salary}`
+  else return `£${salary * 10}`
+ }
 
-//Feb-03-2021 - 30
-//
+//Feb-04-2021 - 30
+//Find the first non-consecutive number
+function firstNonConsecutive (arr) {
+  for (let i = 1; i < arr.length; i++) {
+        if (arr[i - 1] + 1 !== arr[i]) return arr[i];
+    }
+    return null;
+}
+
