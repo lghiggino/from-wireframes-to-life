d24e4950bded8a5dc8e0be96c19d4b7159f069e8
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-02-06 18:59:45 -0300

    tired of CW

--- Due-Feb-04-2021/codewars.js
+++ Due-Feb-04-2021/codewars.js
@@ -509,3 +509,73 @@ function firstNonConsecutive (arr) {
     return null;
 }
 
+//Feb-05-2021 - 31
+//Holiday III - Fire on the boat
+function fireFight(str){
+  str = str.split(" ")
+  for (let i = 0; i < str.length; i++){
+    if (str[i] === "Fire"){
+      str[i] = "~~"
+    }
+  }
+  return str.join(" ")
+}
+
+//best
+const fireFightBest = s =>
+  s.replace(/Fire/g, `~~`);
+
+//Feb-05-2021 - 32
+//Element equals its index
+function indexEqualsValue(a) {
+  a.forEach((el,idx) => {
+    console.log(el, idx, el === idx)
+    if(el === idx){
+      return idx
+    }
+  })
+  return -1;
+}
+
+//best with superfast method
+function indexEqualsValueFast(a) {
+  let i = 0
+  for (; i<a.length; i++){
+     if (i === a[i]){
+       break; 
+     }
+   }
+   return i === a.length ? -1 : i; 
+ }
+
+//Feb-05-2021 - 33
+//My head is at the wrong end!
+function fixTheMeerkat(arr) {
+  return arr.reverse();
+}
+
+//Feb-06-2021 - 34
+//Remove the minimum
+function removeSmallest(numbers) {
+  if (numbers.length == 0){ return []}
+  else{
+    let smallest = numbers[0]
+    let index = 0
+    for (let i = 1; i < numbers.length; i++){
+      if(numbers[i] < smallest){
+        smallest = numbers[i]
+        index = i
+      }
+    }
+    numbers.splice(index,1)
+    return numbers
+  }
+}
+
+//Remove the minimum best
+const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
+//Feb-06-2021 - 35
+//
+
+//Feb-06-2021 - 36
+//
