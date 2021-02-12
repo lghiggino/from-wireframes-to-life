d65645baa442522118983446f5a276c19572d895
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-02-12 19:16:16 -0300

    delete button functionality

--- Dues/2021-Feb/Due-Feb-11-2021/local-storage-botScore-button/js/main.js
+++ Dues/2021-Feb/Due-Feb-11-2021/local-storage-botScore-button/js/main.js
@@ -2,34 +2,42 @@
 const submitBtn = document.getElementById("submitISBN")
 const bookShelf = document.getElementById("book-shelf")
 const bookTitle = document.getElementById("book-title")
-// const bookAuthor = document.getElementById("book-author")
 const firstLine = document.getElementById("first-line")
 
-//renderShelf()
+function checkForExistingBookLibrary(){
+    if (!localStorage.getItem("bookLibrary")){
+        localStorage.setItem("bookLibrary", "")
+    }
+    renderShelf()
+}
+
+checkForExistingBookLibrary()
+
+
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault()
-    let choice = document.getElementById("bookISBN").value
-    if(!localStorage.getItem("bookLibrary")){
-        fetchOpenLibrary(choice)
-    }else if(localStorage.bookLibrary.includes(choice)){
+    let bookISBN = document.getElementById("bookISBN").value
+    if(localStorage.bookLibrary.includes(bookISBN)){
         renderShelf()
-        throw new Error ("That book is already present in your local book shelf")
+        alert("That book is already present in your local book library")
+        throw new Error ("That book is already present in your local book library")
     }else {
-        fetchOpenLibrary(choice)
+        fetchOpenLibrary(bookISBN)
     }
 })
 
-async function fetchOpenLibrary(choice) {
-    const res = await fetch(`https://openlibrary.org/isbn/${choice}.json`);
+async function fetchOpenLibrary(bookISBN) {
+    const res = await fetch(`https://openlibrary.org/isbn/${bookISBN}.json`);
     const data = await res.json();
-    createBookInfoCard(data, choice)
+    console.log(data, bookISBN)
+    createBookInfoCard(data, bookISBN)
 }
 
-function createBookInfoCard(data, choice){
+function createBookInfoCard(data, bookISBN){
     const title = data.title;
     let bookInfoCard = `
-        <article class="book-card" id="book-id-${choice}">
+        <article class="book-card" id="book-id-${bookISBN}">
             <h3 class="book-title">${title}</h3>
             <button class="delete">X</button>
         </article>
@@ -38,36 +46,41 @@ function createBookInfoCard(data, choice){
 }
 
 function appendToLocalStorage(bookInfoCard){
-    if (!localStorage.getItem("bookLibrary")){
-    localStorage.setItem("bookLibrary", bookInfoCard)
+    let aditionalBookCard = localStorage.getItem("bookLibrary") + "," + bookInfoCard
+    localStorage.setItem("bookLibrary", aditionalBookCard)
     renderShelf()
-    }else {
-        let aditionalBookCard = localStorage.getItem("bookLibrary") + "," + bookInfoCard
-        localStorage.setItem("bookLibrary", aditionalBookCard)
-        renderShelf()
-    }
 }
-
 
 function renderShelf(){
     let storageArray = localStorage.bookLibrary.split(",")
     bookShelf.innerHTML = "";
     storageArray.forEach(book => {
         bookShelf.innerHTML += book;
-    })    
+    })   
+    generateDeleteButton()
 }
+
+function generateDeleteButton(){
+    let allDeleteButtons = document.querySelectorAll(".delete")
+    allDeleteButtons.forEach(button => {
+        button.addEventListener("click", (e) =>{
+            console.log(e.target.parentElement.id) 
+            let storageArray = localStorage.bookLibrary.split(",")
+            storageArray.forEach( (el, idx) => {
+                if (el.indexOf(e.target.parentElement.id) >= 0){
+                    console.log(storageArray[idx])
+                    storageArray.splice(idx,1)
+                }
+            })
+            console.log(storageArray)
+            localStorage.setItem("bookLibrary", storageArray.join(","))
+            renderShelf()
+        })
+    })
+}
+  
 //9780140328721
 //9783161484100
+//9781405358071
 
 //console.log(localStorage)
-
-// function fetchOpenLibrary(choice){
-//     let apiAdress = `https://openlibrary.org/isbn/${choice}.json`
-//     fetch(apiAdress)
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(data)
-//             createBookInfoCard(data)
-//         })
-//         .catch(err => console.log(err))
-// }
