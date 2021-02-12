cdcf26dc0cdf1137304eb5741545fe1d264e2533
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-02-12 08:20:17 -0300

    uncle bobs refactor

--- Dues/2021-Feb/Due-Feb-11-2021/local-storage-botScore-button/js/main.js
+++ Dues/2021-Feb/Due-Feb-11-2021/local-storage-botScore-button/js/main.js
@@ -5,66 +5,64 @@ const bookTitle = document.getElementById("book-title")
 // const bookAuthor = document.getElementById("book-author")
 const firstLine = document.getElementById("first-line")
 
-renderShelf()
+//renderShelf()
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault()
     let choice = document.getElementById("bookISBN").value
-    console.log(choice)
+    if(!localStorage.getItem("bookLibrary")){
+        fetchOpenLibrary(choice)
+    }else if(localStorage.bookLibrary.includes(choice)){
+        renderShelf()
+        throw new Error ("That book is already present in your local book shelf")
+    }else {
+        fetchOpenLibrary(choice)
+    }
+})
 
+function fetchOpenLibrary(choice){
+    console.log(choice)
     let apiAdress = `https://openlibrary.org/isbn/${choice}.json`
     fetch(apiAdress)
         .then(res => res.json())
         .then(data => {
-            //check if book is already at localStorage
-            // if(!localStorage.getItem("bookLibrary")){
-                
-            // }
-            // else if (localStorage.bookLibrary.includes(`book-id-${choice}`)){
-            //     console.log("book is already in library")
-            //     return
-            // }else{
-                
-                console.log(data);
-                const title = data.title;
-
-                let bookInfo = `
-                    <article class="book-card" id="book-id-${choice}">
-                        <h3 class="book-title">${title}</h3>
-                        <button class="delete">X</button>
-                    </article>
-                `
-
-                bookShelf.innerHTML = localStorage.bookInfo
-
-                //store
-                if (!localStorage.getItem("bookLibrary")){
-                    localStorage.setItem("bookLibrary", bookInfo)
-                }else {
-                    let aditionalBook = localStorage.getItem("bookLibrary") + "," + bookInfo
-                    localStorage.setItem("bookLibrary", aditionalBook)
-                }
-
-                renderShelf()
-            //}
+            console.log(data)
+            createBookInfoCard(data)
         })
-        .catch(err => console.log(err))
-})
+        .catch(err => {throw new Error(err)})
+}
+
+function createBookInfoCard(data){
+    const title = data.title;
+    let bookInfoCard = `
+        <article class="book-card" id="book-id-${choice}">
+            <h3 class="book-title">${title}</h3>
+            <button class="delete">X</button>
+        </article>
+    `
+    appendToLocalStorage(bookInfoCard)
+}
+
+function appendToLocalStorage(bookInfoCard){
+    if (!localStorage.getitem("bookLibrary")){
+    localStorage.setItem("bookLibrary", bookInfoCard)
+    renderShelf()
+    }else {
+        let aditionalBookCard = localStorage.getItem("bookLibrary") + "," + bookInfoCard
+        localStorage.setItem("bookLibrary", aditionalBookCard)
+        renderShelf()
+    }
+}
+
+
 
 
 function renderShelf(){
-    if (!localStorage.getItem("bookLibrary")){
-        return
-    }else{
-        let storageArray = localStorage.bookLibrary.split(",")
-        bookShelf.innerHTML = "";
-        storageArray.forEach(book => {
-            bookShelf.innerHTML += book;
-        })
-       
-    }
-    
-
+    let storageArray = localStorage.bookLibrary.split(",")
+    bookShelf.innerHTML = "";
+    storageArray.forEach(book => {
+        bookShelf.innerHTML += book;
+    })    
 }
 //9780140328721
 //9783161484100
