45a44d37510edbc1c4c3413fe410edb92f1e1693
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-03-03 20:32:37 -0300

    pokemonAPI

--- Dues/2021-Mar/Due-Mar-02-2021/PokemonAPI/js/main.js
+++ Dues/2021-Mar/Due-Mar-02-2021/PokemonAPI/js/main.js
@@ -1,18 +1,35 @@
 console.log("JS is on");
 
 const main = document.querySelector("main")
+const allCards = document.getElementById("all-cards")
 const input = document.querySelector("input")
 const submit = document.querySelector("button")
-submit.addEventListener("click", makeFetch)
+window.addEventListener("DOMContentLoaded", fetchAll)
 
 let pokemonData
 
-async function makeFetch(){
-    const apiURL = `https://pokeapi.co/api/v2/pokemon/${input.value}`
-    const response = await fetch(apiURL);  
+async function fetchAll(){
+    const apiFirst50Pokemon = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0"
+    const response = await fetch(apiFirst50Pokemon);  
     const data = await response.json()
     //do something with data
-    pokemonData = data
+    pokemonData = data.results
+    renderCards(pokemonData)
 }
 
-card = {}
+function renderCards(dataArray){
+    allCards.innerHTML = ""
+    dataArray.forEach(pokemon => {
+        console.log(pokemon)
+    })
+}
+
+
+
+async function singleFetch(){
+   // const apiURL = `https://pokeapi.co/api/v2/pokemon/${input.value}`
+   const response = await fetch(apiURL);
+   const data = await response.json()
+   pokemonData = data.results //?
+   renderCards(pokemonData)
+}
