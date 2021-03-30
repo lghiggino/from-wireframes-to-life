8a8f149dbca694ef4d0530efcdb01da705313606
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-03-29 21:21:25 -0300

    every route is set + morgan + restVsCode

--- Dues/2021-Mar/Due-Mar-30-2021/fullStackOpen/index.js
+++ Dues/2021-Mar/Due-Mar-30-2021/fullStackOpen/index.js
@@ -1,11 +1,25 @@
 const express = require ("express");
 const app = express();
 const PORT = 2222;
+const morgan = require("morgan")
 
-const persons = require ("./persons");
+//database
+let persons = require ("./persons");
 
+//middleware
 app.use(express.json())
+const requestLogger = (request, response, next) => {
+    console.log("Method:", request.method)
+    console.log("Path:", request.path)
+    console.log("Body:", request.body)
+    console.log("-=-=-=-=-")
+    next()
+}
+//app.use(requestLogger)
 
+app.use(morgan('tiny'))
+
+//routing
 app.get("/", (request, response) => {
     response.send("<h1>Hello from Home /</h1>")
     response.end()
@@ -20,9 +34,11 @@ app.get("/api/persons/:id", (request, response) => {
     if (answer){
         response.json(answer)
     }else{
+        response.json("404 - file not found")
         response.status(404)
     }
     response.end()
+    app.use(morgan())
 })
 
 app.get("/info", (request, response) => {
@@ -36,6 +52,36 @@ app.get("/info", (request, response) => {
     response.end()
 })
 
+app.delete("/api/persons/:id", (request, response) => {
+    const requestedId = Number(request.params.id)
+    persons = persons.filter(el => el.id !== requestedId)
+    response.status(204).end()
+})
+
+app.post("/api/persons/:name/:number", (request, response) => {
+    function generateId(){
+        let mappedIds = persons.map(el => el.id)
+        let maxId = Math.max(...mappedIds)
+        return maxId+1
+    }
+    let name = request.params.name
+    let number = request.params.number
+    if(!name || !number){
+        return response.status(404).json({error : "content missing"})
+    }else if(persons.some(el => el.name === name)){
+        return response.status(404).json({error : "name already exists"})
+    }
+    // console.log(name, number)
+    const newPerson = {id:generateId(), name:name, number:number}
+    persons.push(newPerson)
+    response.json(persons)
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: "unknown endpoint"})
+}
+
+app.use(unknownEndpoint)
 
 //INIT the server
 app.listen(PORT, () => {
