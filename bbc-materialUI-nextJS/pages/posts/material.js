e6013fa6f64fd2a5136afa59a0d75ede612f8196
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-05-10 22:03:06 -0300

    paginationvia layout

--- bbc-materialUI-nextJS/pages/posts/material.js
+++ bbc-materialUI-nextJS/pages/posts/material.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+import Layout from '../../components/layout';
 
 export default function MyComponent() {
   const theme = useTheme();
@@ -9,14 +10,12 @@ export default function MyComponent() {
   const isActiveMQ1 = useMediaQuery("(max-width: 200px")
 
   return (
-      <>
-        <h1>App</h1>
-        <span>{`theme.breakpoints.up('sm') matches: ${matches1}`}</span>;
-        {isActiveMQ1 && <p>This MQ shows ONLY when the screen is super small</p>}
-        {matches0 && <p>This is a media query generated paragraph if the theme breakpoint is bigger than XS - which is always!</p>}
-        {matches1 && <p>This is a media query generated paragraph if the theme breakpoint is bigger than SM!</p>}
-        
-        
-      </>
+    <Layout>
+      <h1>App</h1>
+      <span>{`theme.breakpoints.up('sm') matches: ${matches1}`}</span>;
+      {isActiveMQ1 && <p>This MQ shows ONLY when the screen is super small</p>}
+      {matches0 && <p>This is a media query generated paragraph if the theme breakpoint is bigger than XS - which is always!</p>}
+      {matches1 && <p>This is a media query generated paragraph if the theme breakpoint is bigger than SM!</p>}
+    </Layout>
   )
 }
--- bbc-materialUI-nextJS/pages/posts/sandbox.js
+++ bbc-materialUI-nextJS/pages/posts/sandbox.js
@@ -1,7 +1,9 @@
-export default function Sandbox(){
+import Layout from "../../components/layout";
+
+export default function Sandbox() {
     return (
-        <>
+        <Layout>
             <p>This is the sandbox page</p>
-        </>
+        </Layout>
     )
 }
