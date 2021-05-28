decab4919c94328cb1ddc60db28cc8a2ef0a527a
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-05-27 21:05:46 -0300

    layout cleanUp

--- bbc-materialUI-nextJS/components/layout.js
+++ bbc-materialUI-nextJS/components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = "NJS-MUI-BBC"
+const name = "NextJS-MUI-LNG"
 export const siteTitle = "NextJS MaterialUI This is The BBC"
 
 export default function Layout({ children, home }) {
