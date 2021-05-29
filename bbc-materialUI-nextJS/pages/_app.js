a660964f080640bed7f7c049644a51acd853293a
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-05-29 19:26:21 -0300

    nextJS + materialUI config

--- bbc-materialUI-nextJS/pages/_app.js
+++ bbc-materialUI-nextJS/pages/_app.js
@@ -1,7 +1,38 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { CacheProvider } from '@emotion/react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import createCache from '@emotion/cache';
+import theme from '../src/theme';
+
+export const cache = createCache({ key: 'css', prepend: true });
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
 
-export default MyApp
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>NextJs + MaterialUI + UX</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
