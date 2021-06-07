5d792f497389513aec1352dae0f0fa65472882e7
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-06-06 21:51:37 -0300

    testing containers and grid items

--- bbc-materialUI-nextJS/pages/materialUI/containerTest.tsx
+++ bbc-materialUI-nextJS/pages/materialUI/containerTest.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import { Grid, makeStyles } from '@material-ui/core';
+import { Grid, makeStyles, Paper } from '@material-ui/core';
 
 //layout import
 import CustomAppBar from "../../src/CustomAppbar"
@@ -20,14 +20,26 @@ const useStyles = makeStyles({
   outline: {
     outline: "2px solid red",
     width: "100%",
-    height: "85vh",
+    height: "55vh",
   },
   innerGridA: {
     backgroundImage: `url(/images/kittyA.webp)`,
   },
   innerGridB: {
     backgroundImage: `url(/images/kittyB.webp)`,
-  }
+  },
+  rootR: {
+    flexGrow: 1,
+    backgroundColor: "#a6b6c6",
+    height: "auto"
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: 32,
+  },
 });
 
 
@@ -68,7 +80,7 @@ const gridTest = () => {
 
       <Container maxWidth="xl" disableGutters className={classes.outline}>
         <Grid container >
-          <Grid item xs={12} sm={12} md={6} spacing={0} className={classes.innerGridA}>
+          <Grid item xs={12} sm={12} md={6} spacing={2} className={classes.innerGridA}>
             <Typography variant="h1" component="h1">
               Lorem, ipsum dolor.
             </Typography>
@@ -80,7 +92,22 @@ const gridTest = () => {
           <Grid item xs={12} sm={12} md={6} spacing={0} className={classes.innerGridB}>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsum quidem blanditiis inventore tempora, ducimus recusandae aliquid, perspiciatis architecto distinctio, earum culpa eius laudantium quam repellat exercitationem fugit autem dignissimos porro deleniti a. Recusandae natus quaerat sequi quas optio cumque quibusdam dolorum, dignissimos blanditiis expedita odio exercitationem saepe, nobis doloremque facilis deleniti earum! Accusantium tempora molestiae rem totam nostrum provident, consectetur quo quas, fugiat dolorum ipsum suscipit debitis quae in. Expedita commodi sapiente inventore sunt minus debitis. Natus asperiores nulla veritatis quasi cupiditate deserunt, eius sed autem animi. Et ipsa repellat accusamus laboriosam? Temporibus dolor rem cum! Accusamus illum maiores, corporis tempora magni expedita unde? Ab nihil corporis eos velit repellendus optio, eveniet eum dolore, odit excepturi inventore est voluptate tempora mollitia nisi dolorem quos error ullam sit ad ratione praesentium culpa voluptatum. Deleniti corporis cum libero eum eligendi. Aspernatur accusantium nulla quam omnis sunt. Eaque dolorem repudiandae ut, expedita suscipit minima repellat voluptatum nostrum aperiam doloribus et, animi quam inventore accusantium aspernatur! Illo porro distinctio fugiat doloribus, obcaecati deserunt atque? Dolores optio repellendus ipsum asperiores quaerat suscipit impedit odit minima commodi aperiam sunt quis, cumque dolorum quia. Voluptatem unde doloribus optio a nostrum nihil dolores placeat eaque ut quis, laboriosam illum nobis assumenda facilis, exercitationem cumque dignissimos impedit quam eos! Iste pariatur, perferendis rerum odit eius cupiditate est laudantium architecto vitae doloremque possimus cumque, vero nostrum dolore esse beatae. Iusto deserunt, et veritatis alias natus quibusdam sapiente dicta optio, id esse minima molestiae. Vitae voluptate corrupti cum possimus cumque ducimus accusantium, culpa, rerum aliquam saepe hic voluptatum deleniti delectus, quam impedit eligendi reiciendis in praesentium. Ducimus nam saepe delectus, id dicta tempore. Laboriosam ipsum impedit ullam voluptatem possimus dolore maxime assumenda distinctio at rem facilis atque dignissimos, velit libero quas, aperiam doloribus, blanditiis in vel ducimus! Eius, ex minima?</p>
           </Grid>
+        </Grid>
+      </Container>
 
+      <Container maxWidth="xl" className={classes.rootR}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={0}>
+            {[0, 1, 2, 3].map((value) => (
+              <Grid key={value} item>
+                <Paper className={classes.paper}>
+                  <Typography >
+                    {value}
+                  </Typography>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
       </Container>
 
