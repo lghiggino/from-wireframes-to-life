54ea93f9049a5650e0851e2d7b9863ace41a8b97
lghiggino <lghiggino@gmail.com>
2021-06-07 22:40:28 -0300

    materialUI integration

--- bbc-materialUI-nextJS/.storybook/preview.js
+++ bbc-materialUI-nextJS/.storybook/preview.js
@@ -1,3 +1,4 @@
+import { muiTheme } from 'storybook-addon-material-ui'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -8,3 +9,21 @@ export const parameters = {
     },
   },
 }
+
+const newTheme = {
+  themeName: 'Grey Theme',
+  palette: {
+      primary1Color: '#00bcd4',
+      alternateTextColor: '#4a4a4a',
+      canvasColor: '#616161',
+      textColor: '#bdbdbd',
+      secondaryTextColor: 'rgba(255, 255, 255, 0.54)',
+      disabledColor: '#757575',
+      accent1Color: '#607d8b',
+  },
+};
+
+
+export const decorators = [
+muiTheme([newTheme])
+];
