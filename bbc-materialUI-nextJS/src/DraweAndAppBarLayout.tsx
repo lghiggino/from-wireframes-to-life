6ec4c7f97567e8661e7b1454f8fe88ea11be0180
lghiggino <lghiggino@gmail.com>
2021-06-18 21:06:36 -0300

    appBar icons and styling

--- bbc-materialUI-nextJS/src/DraweAndAppBarLayout.tsx
+++ bbc-materialUI-nextJS/src/DraweAndAppBarLayout.tsx
@@ -18,6 +18,8 @@ import ListItemText from "@material-ui/core/ListItemText";
 import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
 import MenuIcon from "@material-ui/icons/Menu";
+import PersonIcon from '@material-ui/icons/Person';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 //styling
 import utilStyles from '../styles/utils.module.css'
@@ -28,174 +30,194 @@ const drawerWidth = 200;
 
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex"
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1
-  },
-  drawer: {
-    flexShrink: 0,
-    width: drawerWidth
-  },
-  drawerPaper: {
-    width: drawerWidth
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up("md")]: {
-      display: "none"
+    root: {
+        display: "flex"
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1
+    },
+    drawer: {
+        flexShrink: 0,
+        width: drawerWidth
+    },
+    drawerPaper: {
+        width: drawerWidth
+    },
+    title: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+        // determina o momento em que esse icone aparece
+        [theme.breakpoints.up("md")]: {
+            display: "none"
+        }
+    },
+    toolbar: {
+        ...theme.mixins.toolbar,
+        [theme.breakpoints.down("sm")]: {
+            display: "none"
+        }
+    },
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing(3)
+    },
+    header: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: theme.spacing(8),
+    },
+    container: {
+        maxWidth: "66em",
+        padding: "2rem 1rem",
+        margin: "0 auto"
+    },
+    backToHome: {
+        margin: "4rem 0 1rem",
     }
-  },
-  toolbar: {
-    ...theme.mixins.toolbar,
-    [theme.breakpoints.down("sm")]: {
-      display: "none"
-    }
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3)
-  },
-  header: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    marginTop: theme.spacing(8),
-  },
-  container: {
-    maxWidth: "66em",
-    padding: "2rem 1rem",
-    margin: "0 auto"
-  },
-  backToHome: {
-    margin: "4rem 0 1rem",
-  }
 }));
 
 export default function App({ children, home }) {
-  const classes = useStyles();
-  const theme = useTheme();
-  const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
+    const classes = useStyles();
+    const theme = useTheme();
+    const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
 
-  const [open, setOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
 
-  const toggleDrawer = event => {
-    if (
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
+    const toggleDrawer = event => {
+        if (
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            return;
+        }
 
-    setOpen(!open);
-  };
+        setOpen(!open);
+    };
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          {/* MUDAR O CONTEUDO INTERNO DESSSA BARRA PARA O OUTRO COM MENUS */}
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={toggleDrawer}
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Responsive Drawer Example
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant={isMdUp ? "permanent" : "temporary"}
-        classes={{
-          paper: classes.drawerPaper
-        }}
-        anchor="left"
-        open={open}
-        onClose={toggleDrawer}
-      >
-        <div className={classes.toolbar} />
-        <Divider />
-        <List>
-          {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {["All mail", "Trash", "Spam"].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        {/*<Toolbar />  Essa toolbar faz com que o conteudo seja abaixado pela altura de uma toolbar */}
-        <header className={classes.header}>
-          {home ? (
-            <>
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={144}
-                width={144}
-                alt={name}
-              />
-              <h1 className={utilStyles.heading2Xl}>{name}</h1>
-            </>
-          ) : (
-              <>
-                <Link href="/">
-                  <a>
-                    <Image
-                      priority
-                      src="/images/profile.jpg"
-                      className={utilStyles.borderCircle}
-                      height={98}
-                      width={98}
-                      alt={name}
-                    />
-                  </a>
-                </Link>
-                <h2 className={utilStyles.headingLg}>
-                  <Link href="/">
-                    <a className={utilStyles.colorInherit}>{name}</a>
-                  </Link>
-                </h2>
-              </>
-            )}
-        </header>
-        <section className={classes.container}>
-          {children}
-        </section>
-        <footer>
-          {!home && (
-            <div className={classes.backToHome}>
-              <Link href="/">
-                <a>← Back to home</a>
-              </Link>
-            </div>
-          )}
-        </footer>
-      </main>
-    </div>
-  );
+    return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={toggleDrawer}
+                        className={classes.menuButton}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" noWrap className={classes.title}>
+                        Material-UI, NextJS, Typescript experimental site
+                    </Typography>
+                    
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="end"
+                        onClick={() => { console.log("clicou em login") }}
+                    >
+                        <NotificationsIcon />
+                    </IconButton>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="end"
+                        onClick={() => { console.log("clicou em login") }}
+                    >
+                        <PersonIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                className={classes.drawer}
+                variant={isMdUp ? "permanent" : "temporary"}
+                classes={{
+                    paper: classes.drawerPaper
+                }}
+                anchor="left"
+                open={open}
+                onClose={toggleDrawer}
+            >
+                <div className={classes.toolbar} />
+                <Divider />
+                <List>
+                    {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                <List>
+                    {["All mail", "Trash", "Spam"].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                            </ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <main className={classes.content}>
+                {/*<Toolbar />  Essa toolbar faz com que o conteudo seja abaixado pela altura de uma toolbar */}
+                <header className={classes.header}>
+                    {home ? (
+                        <>
+                            <Image
+                                priority
+                                src="/images/profile.jpg"
+                                className={utilStyles.borderCircle}
+                                height={144}
+                                width={144}
+                                alt={name}
+                            />
+                            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/">
+                                <a>
+                                    <Image
+                                        priority
+                                        src="/images/profile.jpg"
+                                        className={utilStyles.borderCircle}
+                                        height={98}
+                                        width={98}
+                                        alt={name}
+                                    />
+                                </a>
+                            </Link>
+                            <h2 className={utilStyles.headingLg}>
+                                <Link href="/">
+                                    <a className={utilStyles.colorInherit}>{name}</a>
+                                </Link>
+                            </h2>
+                        </>
+                    )}
+                </header>
+                <section className={classes.container}>
+                    {children}
+                </section>
+                <footer>
+                    {!home && (
+                        <div className={classes.backToHome}>
+                            <Link href="/">
+                                <a>← Back to home</a>
+                            </Link>
+                        </div>
+                    )}
+                </footer>
+            </main>
+        </div>
+    );
 }
