cc442ee8f750085d797966d737028b8ea712e01e
lghiggino <lghiggino@gmail.com>
2021-07-12 21:07:49 -0300

    charts index page

--- bbc-materialUI-nextJS/pages/charts/index.tsx
+++ bbc-materialUI-nextJS/pages/charts/index.tsx
@@ -1,10 +1,18 @@
 import Layout2 from "../../src/DraweAndAppBarLayout"
+import Link from "next/link"
 
 export default function index() {
 
     return (
         <Layout2 home={false}>
-            teste charts page
+            <ul>
+                <li><Link href="/charts/rechartsPOC">POC Recharts</Link></li>
+                <li><Link href="/charts/pieChart">Pie Chart</Link></li>
+                <li><Link href="/charts/radialBar">Radial Bar Chart</Link></li>
+            </ul>
+
+            <Link href="/charts">back to charts index</Link>
         </Layout2>
     )
 }
+
